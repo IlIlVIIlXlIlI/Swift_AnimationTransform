035fafcf6ae2acacbd3b1fc3160d49e23918ac08
Shogo <vanitas710@yahoo.co.jp>
2021-02-03 16:48:45 +0900

    トランスフォームの足し合わせができた

--- AnimationTransform/ViewController.swift
+++ AnimationTransform/ViewController.swift
@@ -9,6 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapView(_ sender: UITapGestureRecognizer) {
+        // フラワーを作る
+        let flower = UIImageView(image: UIImage(named: "flower"))
+        // 透明度を0にする
+        flower.alpha = 0
+        
+        // 0.2倍サイズにする
+        let scaleTransform = CGAffineTransform(scaleX: 0.2, y:0.2)
+        // -Py/2回転する
+        let rotationTransform = CGAffineTransform(rotationAngle: CGFloat(-Double.pi/2))
+        // トランスフォームを足し合わせる
+        let transform = scaleTransform.concatenating(rotationTransform)
+        // flowerを変形させる
+        flower.transform = transform
+        
+        // タップされた座標にflowerを追加する
+        flower.center = sender.location(in: self.view)
+        view.addSubview(flower)
+        
+        // アニメーション
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       options: [.curveEaseInOut],
+                       animations: {
+                        // 透明度を1にする
+                        flower.alpha = 1.0
+                        // 変形を戻す
+                        flower.transform = .identity
+                       },
+                       completion: nil
+                       )
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
